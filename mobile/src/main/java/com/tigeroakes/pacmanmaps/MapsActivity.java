89e95ca354ead165f7a684cf7302a1301aa944a1
Sherry Yuan <sherry-yxy@hotmail.com>
2016-02-27 21:03:47 -0800

    changed xml files a little

--- mobile/src/main/java/com/tigeroakes/pacmanmaps/MapsActivity.java
+++ mobile/src/main/java/com/tigeroakes/pacmanmaps/MapsActivity.java
@@ -33,7 +33,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
 				.findFragmentById(R.id.map);
 		mapFragment.getMapAsync(this);
-		createTextOverlay();
+		// createTextOverlay();
 	}
 
 	private void createTextOverlay() {
