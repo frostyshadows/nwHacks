fb7ea278d6332ef13a4921329047a11bb35040dd
Tiger Oakes <tigeroakes@gmail.com>
2016-02-28 09:42:33 -0800

    Hid walkway paths from map

--- mobile/src/main/java/com/tigeroakes/pacmanmaps/Paths.java
+++ mobile/src/main/java/com/tigeroakes/pacmanmaps/Paths.java
@@ -30,13 +30,13 @@ public class Paths {
 				new LatLng(-123.25067, 49.26168),
 				new LatLng(-123.25053, 49.26169),
 				new LatLng(-123.25042, 49.26171)
-		)));
+		).visible(false)));
 		d.add(map.addPolyline(new PolylineOptions().add(
 				new LatLng(-123.25120, 49.26384),
 				new LatLng(-123.25126, 49.26378),
 				new LatLng(-123.25166, 49.26364),
 				new LatLng(-123.25167, 49.26364)
-		)));
+		).visible(false)));
 		d.add(map.addPolyline(new PolylineOptions().add(
 				new LatLng(-123.25063, 49.26402),
 				new LatLng(-123.25041, 49.26377),
@@ -58,7 +58,7 @@ public class Paths {
 				new LatLng(-123.25034, 49.26279),
 				new LatLng(-123.25032, 49.26282),
 				new LatLng(-123.25031, 49.26284)
-		)));
+		).visible(false)));
 		d.add(map.addPolyline(new PolylineOptions().add(
 				new LatLng(-123.25317, 49.26284),
 				new LatLng(-123.25317, 49.26285),
@@ -77,7 +77,7 @@ public class Paths {
 				new LatLng(-123.25132, 49.26208),
 				new LatLng(-123.25129, 49.26208),
 				new LatLng(-123.25126, 49.26207)
-		)));
+		).visible(false)));
 		d.add(map.addPolyline(new PolylineOptions().add(
 				new LatLng(-123.25331, 49.26279),
 				new LatLng(-123.25317, 49.26284),
@@ -92,7 +92,7 @@ public class Paths {
 				new LatLng(-123.25233, 49.26297),
 				new LatLng(-123.25229, 49.26293),
 				new LatLng(-123.25200, 49.26257)
-		)));
+		).visible(false)));
 		d.add(map.addPolyline(new PolylineOptions().add(
 				new LatLng(-123.24994, 49.26113),
 				new LatLng(-123.24970, 49.26122),
@@ -105,7 +105,7 @@ public class Paths {
 				new LatLng(-123.24887, 49.26159),
 				new LatLng(-123.24878, 49.26162),
 				new LatLng(-123.24867, 49.26163)
-		)));
+		).visible(false)));
 		d.add(map.addPolyline(new PolylineOptions().add(
 				new LatLng(-123.25338, 49.26172),
 				new LatLng(-123.25335, 49.26173),
@@ -138,13 +138,13 @@ public class Paths {
 				new LatLng(-123.25161, 49.26210),
 				new LatLng(-123.25092, 49.26231),
 				new LatLng(-123.25068, 49.26239)
-		)));
+		).visible(false)));
 		d.add(map.addPolyline(new PolylineOptions().add(
 				new LatLng(-123.25193, 49.26324),
 				new LatLng(-123.25172, 49.26332),
 				new LatLng(-123.25148, 49.26340),
 				new LatLng(-123.25104, 49.26354)
-		)));
+		).visible(false)));
 		d.add(map.addPolyline(new PolylineOptions().add(
 				new LatLng(-123.24918, 49.26058),
 				new LatLng(-123.24970, 49.26122),
@@ -155,7 +155,7 @@ public class Paths {
 				new LatLng(-123.25148, 49.26340),
 				new LatLng(-123.25167, 49.26364),
 				new LatLng(-123.25184, 49.26385)
-		)));
+		).visible(false)));
 		d.add(map.addPolyline(new PolylineOptions().add(
 				new LatLng(-123.25211, 49.26377),
 				new LatLng(-123.25181, 49.26342),
@@ -165,6 +165,6 @@ public class Paths {
 				new LatLng(-123.25042, 49.26171),
 				new LatLng(-123.24994, 49.26113),
 				new LatLng(-123.24940, 49.26048)
-		)));
+		).visible(false)));
 	}
 }
