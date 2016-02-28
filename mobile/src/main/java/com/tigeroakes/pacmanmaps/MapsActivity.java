6f0fb8fb79868724459c9743c427eb4acfb95018
Sherry Yuan <sherry-yxy@hotmail.com>
2016-02-28 10:01:08 -0800

    no message

--- mobile/src/main/java/com/tigeroakes/pacmanmaps/MapsActivity.java
+++ mobile/src/main/java/com/tigeroakes/pacmanmaps/MapsActivity.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
--- mobile/src/main/java/com/tigeroakes/pacmanmaps/Player.java
+++ mobile/src/main/java/com/tigeroakes/pacmanmaps/Player.java
@@ -7,9 +7,13 @@ import com.google.android.gms.maps.model.LatLng;
 /**
  * Created by sherryyuan on 16-02-28.
  */
-public class Player {
+public class Player extends Location {
     private LatLng pos;
     public int PointPerPellet = 100; //why isn't this in pellets class instead??
+
+    public Player(String provider) {
+        super(provider);
+    }
 
     public void updatePlayer() {
     }
